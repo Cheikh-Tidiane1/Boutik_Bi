b0c176a2e613e486fc368fd0d54e3071e996cb6c
cheikh tidiane <cheikh951812@gmail.com>
2023-11-06 13:19:03 +0100

    Boutik-Bi

---
+++ src/Classe/Search.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Classe;
+
+use App\Entity\Category;
+
+class Search
+{
+    /**
+     * @var string
+     */
+    public $string = '';
+
+    /**
+     * @var Category[]
+     */
+    public $categories = [] ;
+
+    // creation de la classe search qui me permet de representer mon object de recherche
+}
